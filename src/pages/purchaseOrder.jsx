e9dccbee751a9e7a4d64614090578645cc2c413a
kayoJr <kalebmelaku97@gmail.com>
2023-08-16 11:39:29 +0300

    order reciept done

--- src/pages/purchaseOrder.jsx
+++ src/pages/purchaseOrder.jsx
@@ -3,12 +3,13 @@ import Header from '../components/header';
 
 function Order()
 {
-    function handlePrint() {
+    function handlePrint()
+    {
         window.print();
-      }
+    }
     return (
         <div className='purchase'>
-             <button className='printBtn btn' onClick={handlePrint}>Print</button>
+            <button className='printBtn btn' onClick={handlePrint}>Print</button>
             <Header page="PURCHASE ORDER" />
             <div className="date">
                 <p>Date:</p>
@@ -18,13 +19,13 @@ function Order()
                 <div className="items">
                     <div className="item">
                         <h3>issued by: </h3>
-                        <hr className='first'/>
+                        <hr className='first' />
                         <hr />
                         <hr />
                     </div>
                     <div className="item">
                         <h3>issued to: </h3>
-                        <hr className='first'/>
+                        <hr className='first' />
                         <hr />
                         <hr />
                     </div>
@@ -37,26 +38,27 @@ function Order()
                     </div>
                     <div className="item">
                         <h3>additional instructions: </h3>
-                        <hr className='first'/>
+                        <hr className='first' />
                         <hr />
                         <hr />
                         <hr />
                     </div>
                     <div className="item">
                         <h3>shipping terms: </h3>
-                        <hr className='first'/>
+                        <hr className='first' />
                     </div>
                     <div className="item">
                         <h3>currency & payment terms: </h3>
-                        <hr className='first'/>
+                        <hr className='first' />
                     </div>
                 </div>
                 <div className="delivery">
-                    <h3>DELIVERY DATE: </h3> <hr className='first'/>
+                    <h3>DELIVERY DATE: </h3> <hr className='first' />
                 </div>
             </div>
-            <table className="table" border={1}>
-                <thead>
+            <table className="table" border={0}>
+                <tbody>
+
                     <tr>
                         <td>S.N</td>
                         <td>Description</td>
@@ -65,8 +67,7 @@ function Order()
                         <td>Unit Cost</td>
                         <td>Extended Cost</td>
                     </tr>
-                </thead>
-                <tbody>
+
                     <tr>
                         <td></td>
                         <td></td>
@@ -122,9 +123,65 @@ function Order()
                         <td></td>
                         <td></td>
                         <td></td>
+                    </tr>
+                    <tr className='total_table'>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td className='total total_title'><p>Total</p></td>
+                        <td className='total'><p></p></td>
+                    </tr>
+                    <tr className='total_table'>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td className='total total_title'><p>Taxes</p></td>
+                        <td className='total'><p></p></td>
+                    </tr>
+                    <tr className='total_table'>
+                        <td></td>
+                        <td></td>
+                        <td className='total'><p>PREPARED FOR OWDA PR#</p></td>
+                        <td></td>
+                        <td className='total total_title'><p>Shipping</p></td>
+                        <td className='total'><p></p></td>
+                    </tr>
+                    <tr className='total_table'>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td className='total total_title'><p>Insurance</p></td>
+                        <td className='total'><p></p></td>
+                    </tr>
+                    <tr className='total_table'>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td className='total total_title'><p>Total Invoice</p></td>
+                        <td className='total'><p></p></td>
                     </tr>
                 </tbody>
             </table>
+            <div className="signature">
+                <div>
+                    <h2>Authorized on Behalf of OWDA:</h2>
+                    <hr />
+                    <p>(Name/title/date/sign)</p>
+                </div>
+                <div>
+                    <h2>Acceptance on Behalf of supplier</h2>
+                    <hr />
+                    <p>(Name/title/date/sign)</p>
+                </div>
+            </div>
+            <div className="footer">
+                <p>Please refer to this purchase order number in all correspondence concerning the order. Please supply 2 copies of your invoices</p>
+                <p>See attached terms and conditions</p>
+            </div>
         </div>
     );
 }
